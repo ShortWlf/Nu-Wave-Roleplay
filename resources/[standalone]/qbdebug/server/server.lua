1df5f557938d98df4bd55371706a66081bd2c091
ShortWlf <brianpmdsn@gmail.com>
2022-11-09 19:20:42 -0600

    Update server.lua

--- resources/[standalone]/qbdebug/server/server.lua
+++ resources/[standalone]/qbdebug/server/server.lua
@@ -1,4 +1,5 @@
 -- DEBUG line Copy here
+-- you can change ' Debugger: ' in line 5 to the name of the script you want to print a report
 Citizen.CreateThread(function()
   while true do
     print("Debugger: Hello world!")
