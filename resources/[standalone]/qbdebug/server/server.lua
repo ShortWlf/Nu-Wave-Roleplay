6c22330e74c77de18e6a9cd78dd98b851df5dcdb
ShortWlf <brianpmdsn@gmail.com>
2022-11-09 19:22:10 -0600

    Update server.lua

--- resources/[standalone]/qbdebug/server/server.lua
+++ resources/[standalone]/qbdebug/server/server.lua
@@ -1,4 +1,4 @@
--- DEBUG line Copy here
+-- DEBUG line Copy here and put into any server script to make it post a print report the server console
 -- you can change ' Debugger: ' in line 5 to the name of the script you want to print a report
 Citizen.CreateThread(function()
   while true do
