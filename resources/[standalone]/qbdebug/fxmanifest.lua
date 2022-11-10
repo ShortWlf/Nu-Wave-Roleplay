bddbec884ec709192540fa2af679f9761a6bc4bc
ShortWlf <brianpmdsn@gmail.com>
2022-11-09 18:35:31 -0600

    Create fxmanifest.lua

---
+++ resources/[standalone]/qbdebug/fxmanifest.lua
@@ -0,0 +1,14 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'shortwlf'
+description 'qbdebug'
+version '1.0.0'
+
+client_scripts {
+    'client/client.lua'
+}
+server_scripts {
+    'server/server.lua'
+}
